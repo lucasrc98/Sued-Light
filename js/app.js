41f91b02822a873ecc96b75225d343f6dc615001
Lucas Rodrigues <lucasrchaves1@gmail.com>
2019-05-09 19:19:21 -0300

    Setando variaveis de perguntas e respostas padrões

--- js/app.js
+++ js/app.js
@@ -14,3 +14,18 @@ const respostas = [
 	"Passe amanhã",
 	"Volta outro dia"
 ]
+
+let elementoEntrada = document.getElementById("inputField");
+let elementoSaida = document.getElementById("answer");
+
+let resposta = "";
+let ocultar = false;
+let contador = 0;
+
+/*Passando uma pergunta aleatoria do banco de perguntas para a variavel perguntaPadrao
+que sera utilizada para fingir que o usuario está digitando uma pergunta, depois que o botão chave for apertado */
+let perguntaPadrao = perguntas[Math.floor(Math.random() * perguntas.length)];
+
+/*Passando uma resposta aleatoria do banco de respostas para a variavel respostaPadrao
+que sera utilizada quando o usuario fizer uma pergunta mas nao tiver apertado o botão chave antes */
+let respostaPadrao = respostas[Math.floor(Math.random() * respostas.length)];
