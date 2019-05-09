06ce158eee0e45cb70ae38ec7b88804e1a994040
Lucas Rodrigues <lucasrchaves1@gmail.com>
2019-05-09 18:39:53 -0300

    Criando o banco de perguntas e respostas padrões do app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 alert('Bem vindo mortal, me faça uma pergunta');    
+
+//Array com as perguntas padrões 
+const perguntas = [
+	"Grande Sued, me ajude a descobrir",
+	"Lord da sabedoria, me diga",
+	"Seria possivel me responder"
+]
+
+//Array com as respostas padrões 
+const respostas = [
+	"Hoje é meu dia de folga",
+	"Não recebo para responder esse tipo de pergunta",
+	"Passe amanhã",
+	"Volta outro dia"
+]
