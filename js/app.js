3445076a149b7552fc4b8526a3ce2e913a97cf67
Lucas Rodrigues <lucasrchaves1@gmail.com>
2019-05-10 19:19:15 -0300

    Versão 0 concluída

--- js/app.js
+++ js/app.js
@@ -24,8 +24,37 @@ let contador = 0;
 
 /*Passando uma pergunta aleatoria do banco de perguntas para a variavel perguntaPadrao
 que sera utilizada para fingir que o usuario está digitando uma pergunta, depois que o botão chave for apertado */
-let perguntaPadrao = perguntas[Math.floor(Math.random() * perguntas.length)];
+let perguntaPadrao = perguntas[Math.floor(Math.random() * perguntas.length)]
 
 /*Passando uma resposta aleatoria do banco de respostas para a variavel respostaPadrao
 que sera utilizada quando o usuario fizer uma pergunta mas nao tiver apertado o botão chave antes */
-let respostaPadrao = respostas[Math.floor(Math.random() * respostas.length)];
+let respostaPadrao = respostas[Math.floor(Math.random() * respostas.length)]
+
+elementoEntrada.addEventListener("input", (e) =>{
+	let element = e.target;
+	if (e.data == ";") {
+		ocultar = !ocultar;
+		
+		element.value = element.value.substring(0, element.value.length - 1)
+		return;
+	}
+	if(ocultar){
+		if(e.inputType == "deleteContentBackward"){
+			resposta = resposta.substring(0, resposta.value.length - 1)
+			contador = ((contador - 1)< 0) ? 0 : contador --;
+			return;
+		}
+		element.value = element.value.substring(0, element.value.length - 1) +
+			perguntaPadrao.charAt(contador ++)
+		resposta += e.data;
+	}
+
+})
+function mostraResposta(){
+	let elementoResposta = document.getElementById("answer");
+	if (contador <= 0) {
+		elementoResposta.innerText = respostaPadrao;
+	}else{
+		elementoResposta.innerText = resposta;
+	}
+}
